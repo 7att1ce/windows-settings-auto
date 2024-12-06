d612d85d4746a17c1df9359276207bd5fc3b30bd
Lattice <65119505+7att1ce@users.noreply.github.com>
2024-12-06 02:40:52 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -39,6 +39,281 @@ def windows_settings_auto():
                        Settings.System.P):
             pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
 
+    def gaming_auto():
+        pyautogui.click(x=Settings.Gaming.P[0], y=Settings.Gaming.P[1], interval=DEFAULT_INTERVAL)
+
+        for (x, y) in (Settings.Gaming.GameBar.P,
+                       Settings.Gaming.GameBar.YunXuKongZhiQiDaKaiGameBar.P,
+                       Settings.Gaming.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
+    def accessibility_auto():
+        pyautogui.click(x=Settings.Accessibility.P[0], y=Settings.Accessibility.P[1], interval=DEFAULT_INTERVAL)
+
+        for (x, y) in (Settings.Accessibility.ShuBiaoZhiZhenYuChuKong.P,
+                       Settings.Accessibility.ShuBiaoZhiZhenYuChuKong.ChuKongXianShiQi.ShiYuanQuanGengShenGengDa.P,
+                       Settings.Accessibility.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
+        for (x, y) in (Settings.Accessibility.JiangShuRen.P,
+                       Settings.Accessibility.JiangShuRen.JiangShuRenDeKuaiJieFangShi.P,
+                       Settings.Accessibility.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
+        pyautogui.click(Settings.Blank.P[0], Settings.Blank.P[1], interval=DEFAULT_INTERVAL)
+        pyautogui.scroll(-10000)
+        time.sleep(DEFAULT_INTERVAL)
+        for (x, y) in (Settings.Accessibility.JianPan.P,
+                       Settings.Accessibility.JianPan.NianZhiJian.P,
+                       Settings.Accessibility.JianPan.NianZhiJian.NianZhiJianDeJianPanKuaiJieFangShi.P,
+                       Settings.Accessibility.JianPan.NianZhiJian.ZaiHangZhongAnLiangCiShiSuoDingKuaiJieJian.P,
+                       Settings.Back.P,
+                       Settings.Accessibility.JianPan.ShaiXuanJian.P,
+                       Settings.Accessibility.JianPan.ShaiXuanJian.ShaiXuanJianDeJianPanKuaiJieFangShi.P,
+                       Settings.Accessibility.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
+    def privacy_and_security_auto():
+        pyautogui.click(Settings.PrivacyAndSecurity.P[0], Settings.PrivacyAndSecurity.P[1], interval=DEFAULT_INTERVAL)
+
+        for (x, y) in (Settings.PrivacyAndSecurity.ChangGui.P,
+                       Settings.PrivacyAndSecurity.ChangGui.YunXuWangZhanTongGuoFangWenWoDeYuYanLieBiaoLaiXianShiBenDiXiangGuanNeiRong.P,
+                       Settings.PrivacyAndSecurity.ChangGui.YunXuWindowsGenZongYingYongQiDongYiGaiJinKaiShiHeSouSuoNeiRong.P,
+                       Settings.PrivacyAndSecurity.ChangGui.ZaiSheZhiYingYongZhongWeiWoXianShiJianYiDeNeiRong.P,
+                       Settings.PrivacyAndSecurity.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
+        for (x, y) in (Settings.PrivacyAndSecurity.MoJiShuXieHeJianRuGeXingHua.P,
+                       Settings.PrivacyAndSecurity.MoJiShuXieHeJianRuGeXingHua.ZiDingYiMoJiShuXieHeJianRuCiDian.P,
+                       Settings.PrivacyAndSecurity.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
+        for (x, y) in (Settings.PrivacyAndSecurity.ZhenDuanHeFanKui.P,
+                       Settings.PrivacyAndSecurity.ZhenDuanHeFanKui.FanKuiPinLv.P,
+                       Settings.PrivacyAndSecurity.ZhenDuanHeFanKui.FanKuiPinLv.CongBu.P,
+                       Settings.PrivacyAndSecurity.ZhenDuanHeFanKui.ShanChuZhenDuanShuJu.P,
+                       Settings.PrivacyAndSecurity.ZhenDuanHeFanKui.ShanChuZhenDuanShuJu.ShanChu.P,
+                       Settings.PrivacyAndSecurity.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
+        for (x, y) in (Settings.PrivacyAndSecurity.HuoDongLiShiJiLu.P,
+                       Settings.PrivacyAndSecurity.HuoDongLiShiJiLu.HuoDongLiShiJiLu.ZaiCiSheBeiShangCunChuWoDeHuoDongLiShiJiLu.P,
+                       Settings.PrivacyAndSecurity.HuoDongLiShiJiLu.QingChuCiZhangHuDeHuoDongLiShiJiLu.P,
+                       Settings.PrivacyAndSecurity.HuoDongLiShiJiLu.QingChuCiZhangHuDeHuoDongLiShiJiLu.QingChu.P,
+                       Settings.PrivacyAndSecurity.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
+        for (x, y) in (Settings.PrivacyAndSecurity.SouSuoQuanXian.P,
+                       Settings.PrivacyAndSecurity.SouSuoQuanXian.AnQuanSouSuo.GuanBi.P,
+                       Settings.PrivacyAndSecurity.SouSuoQuanXian.YunNeiRongSouSuo.MicrosoftZhangHu.P,
+                       Settings.PrivacyAndSecurity.SouSuoQuanXian.YunNeiRongSouSuo.GongZuoHuoXueXiaoZhangHu.P,
+                       Settings.PrivacyAndSecurity.SouSuoQuanXian.LiShiJiLu.CiSheBeiShangDeSouSuoLiShiJiLu.P,
+                       Settings.PrivacyAndSecurity.SouSuoQuanXian.LiShiJiLu.QingChuSheBeiSouSuoLiShiJiLu.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+        pyautogui.click(Settings.Blank.P[0], Settings.Blank.P[1], interval=DEFAULT_INTERVAL)
+        pyautogui.scroll(-10000)
+        time.sleep(DEFAULT_INTERVAL)
+        for (x, y) in (Settings.PrivacyAndSecurity.SouSuoQuanXian.GengDuoSheZhi.XianShiSouSuoYaoDian.P,
+                       Settings.PrivacyAndSecurity.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+        
+        for (x, y) in (Settings.PrivacyAndSecurity.WeiZhi.P,
+                       Settings.PrivacyAndSecurity.WeiZhi.YunXuYingYongFangWenWeiZhi.P,
+                       Settings.PrivacyAndSecurity.WeiZhi.YunXuWeiZhiFuGai.P,
+                       Settings.PrivacyAndSecurity.WeiZhi.WeiZhiLiShiJiLu.P,
+                       Settings.PrivacyAndSecurity.WeiZhi.ZaiYingYongQingQiuWeiZhiShiFaChuTongZhi.P,
+                       Settings.PrivacyAndSecurity.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
+        pyautogui.click(Settings.Blank.P[0], Settings.Blank.P[1], interval=DEFAULT_INTERVAL)
+        pyautogui.scroll(-777)
+        time.sleep(DEFAULT_INTERVAL)
+        for (x, y) in (Settings.PrivacyAndSecurity.YuYinJiHuo.P,
+                       Settings.PrivacyAndSecurity.YuYinJiHuo.YunXuYingYongFangWenYuYinJiHuoFuWu.P,
+                       Settings.PrivacyAndSecurity.YuYinJiHuo.YunXuYingYongFangWenYuYinJiHuoFuWu.YunXuYingYongZaiSheBeiSuoDingShiShiYongYuYinJiHuoFuWu.P,
+                       Settings.PrivacyAndSecurity.YuYinJiHuo.YunXuYingYongFangWenYuYinJiHuoFuWu.Switch.P,
+                       Settings.PrivacyAndSecurity.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
+        pyautogui.click(Settings.Blank.P[0], Settings.Blank.P[1], interval=DEFAULT_INTERVAL)
+        pyautogui.scroll(-777)
+        time.sleep(DEFAULT_INTERVAL)
+        for (x, y) in (Settings.PrivacyAndSecurity.TongZhi.P,
+                       Settings.PrivacyAndSecurity.TongZhi.RangYingYongFangWenTongZhi.P,
+                       Settings.PrivacyAndSecurity.TongZhi.TongZhiFangWenQuanXian.P,
+                       Settings.PrivacyAndSecurity.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
+        pyautogui.click(Settings.Blank.P[0], Settings.Blank.P[1], interval=DEFAULT_INTERVAL)
+        pyautogui.scroll(-777)
+        time.sleep(DEFAULT_INTERVAL)
+        for (x, y) in (Settings.PrivacyAndSecurity.ZhangHuXinXi.P,
+                       Settings.PrivacyAndSecurity.ZhangHuXinXi.YunXuYingYongFangWenNiDeZhangHuXinXi.P,
+                       Settings.PrivacyAndSecurity.ZhangHuXinXi.ZhangHuXinXiFangWenQuanXian.P,
+                       Settings.PrivacyAndSecurity.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
+        pyautogui.click(Settings.Blank.P[0], Settings.Blank.P[1], interval=DEFAULT_INTERVAL)
+        pyautogui.scroll(-777)
+        time.sleep(DEFAULT_INTERVAL)
+        for (x, y) in (Settings.PrivacyAndSecurity.LianXiRen.P,
+                       Settings.PrivacyAndSecurity.LianXiRen.YunXuYingYongFangWenNiDeLianXiRen.P,
+                       Settings.PrivacyAndSecurity.LianXiRen.LianXiRenFangWenQuanXian.P,
+                       Settings.PrivacyAndSecurity.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
+        pyautogui.click(Settings.Blank.P[0], Settings.Blank.P[1], interval=DEFAULT_INTERVAL)
+        pyautogui.scroll(-777)
+        time.sleep(DEFAULT_INTERVAL)
+        for (x, y) in (Settings.PrivacyAndSecurity.RiLi.P,
+                       Settings.PrivacyAndSecurity.RiLi.YunXuYingYongFangWenNiDeRiLi.P,
+                       Settings.PrivacyAndSecurity.RiLi.RiLiFangWenQuanXian.P,
+                       Settings.PrivacyAndSecurity.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
+        pyautogui.click(Settings.Blank.P[0], Settings.Blank.P[1], interval=DEFAULT_INTERVAL)
+        pyautogui.scroll(-777)
+        time.sleep(DEFAULT_INTERVAL)
+        for (x, y) in (Settings.PrivacyAndSecurity.DianHuaHuJiao.P,
+                       Settings.PrivacyAndSecurity.DianHuaHuJiao.YunXuYingYongBoDaDianHua.P,
+                       Settings.PrivacyAndSecurity.DianHuaHuJiao.DianHuaHuJiaoFangWenQuanXian.P,
+                       Settings.PrivacyAndSecurity.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
+        pyautogui.click(Settings.Blank.P[0], Settings.Blank.P[1], interval=DEFAULT_INTERVAL)
+        pyautogui.scroll(-777)
+        time.sleep(DEFAULT_INTERVAL)
+        for (x, y) in (Settings.PrivacyAndSecurity.TongHuaJiLu.P,
+                       Settings.PrivacyAndSecurity.TongHuaJiLu.YunXuYingYongFangWenNiDeHuJiaoLiShiJiLu.P,
+                       Settings.PrivacyAndSecurity.TongHuaJiLu.HuJiaoLiShiJiLuFangWenQuanXian.P,
+                       Settings.PrivacyAndSecurity.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
+        pyautogui.click(Settings.Blank.P[0], Settings.Blank.P[1], interval=DEFAULT_INTERVAL)
+        pyautogui.scroll(-777)
+        time.sleep(DEFAULT_INTERVAL)
+        for (x, y) in (Settings.PrivacyAndSecurity.DianZiYouJian.P,
+                       Settings.PrivacyAndSecurity.DianZiYouJian.YunXuYingYongFangWenNiDeDianZiYouJian.P,
+                       Settings.PrivacyAndSecurity.DianZiYouJian.DianZiYouJianFangWenQuanXian.P,
+                       Settings.PrivacyAndSecurity.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
+        pyautogui.click(Settings.Blank.P[0], Settings.Blank.P[1], interval=DEFAULT_INTERVAL)
+        pyautogui.scroll(-777)
+        time.sleep(DEFAULT_INTERVAL)
+        for (x, y) in (Settings.PrivacyAndSecurity.RenWu.P,
+                       Settings.PrivacyAndSecurity.RenWu.YunXuYingYongFangWenNiDeRenWu.P,
+                       Settings.PrivacyAndSecurity.RenWu.RenWuFangWenQuanXian.P,
+                       Settings.PrivacyAndSecurity.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
+        pyautogui.click(Settings.Blank.P[0], Settings.Blank.P[1], interval=DEFAULT_INTERVAL)
+        pyautogui.scroll(-777)
+        time.sleep(DEFAULT_INTERVAL)
+        for (x, y) in (Settings.PrivacyAndSecurity.XiaoXi.P,
+                       Settings.PrivacyAndSecurity.XiaoXi.YunXuYingYongDuQuDuanXin.P,
+                       Settings.PrivacyAndSecurity.XiaoXi.DuanXinFangWenQuanXian.P,
+                       Settings.PrivacyAndSecurity.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
+        pyautogui.click(Settings.Blank.P[0], Settings.Blank.P[1], interval=DEFAULT_INTERVAL)
+        pyautogui.scroll(-10000)
+        time.sleep(DEFAULT_INTERVAL)
+        for (x, y) in (Settings.PrivacyAndSecurity.WuXianShouFaQi.P,
+                       Settings.PrivacyAndSecurity.WuXianShouFaQi.RangYingYongKongZhiSheBeiWuXianDianShouFaQi.P,
+                       Settings.PrivacyAndSecurity.WuXianShouFaQi.ShouYinJiKongJianFangWenQuanXian.P,
+                       Settings.PrivacyAndSecurity.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
+        pyautogui.click(Settings.Blank.P[0], Settings.Blank.P[1], interval=DEFAULT_INTERVAL)
+        pyautogui.scroll(-10000)
+        time.sleep(DEFAULT_INTERVAL)
+        for (x, y) in (Settings.PrivacyAndSecurity.QiTaSheBei.P,
+                       Settings.PrivacyAndSecurity.QiTaSheBei.YuWeiPeiDuiSheBeiTongXin.P,
+                       Settings.PrivacyAndSecurity.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
+        pyautogui.click(Settings.Blank.P[0], Settings.Blank.P[1], interval=DEFAULT_INTERVAL)
+        pyautogui.scroll(-10000)
+        time.sleep(DEFAULT_INTERVAL)
+        for (x, y) in (Settings.PrivacyAndSecurity.YingYongZhenDuan.P,
+                       Settings.PrivacyAndSecurity.YingYongZhenDuan.YunXuYingYongFangWenYuQiTaYingYongYouGuanDeZhenDuanXinXi.P,
+                       Settings.PrivacyAndSecurity.YingYongZhenDuan.YingYongZhenDuanFangWenQuanXian.P,
+                       Settings.PrivacyAndSecurity.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
+        pyautogui.click(Settings.Blank.P[0], Settings.Blank.P[1], interval=DEFAULT_INTERVAL)
+        pyautogui.scroll(-10000)
+        time.sleep(DEFAULT_INTERVAL)
+        for (x, y) in (Settings.PrivacyAndSecurity.WenDang.P,
+                       Settings.PrivacyAndSecurity.WenDang.RangYingYongFangWenWenDangKu.P,
+                       Settings.PrivacyAndSecurity.WenDang.WenDangKuFangWenQuanXian.P,
+                       Settings.PrivacyAndSecurity.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
+        pyautogui.click(Settings.Blank.P[0], Settings.Blank.P[1], interval=DEFAULT_INTERVAL)
+        pyautogui.scroll(-10000)
+        time.sleep(DEFAULT_INTERVAL)
+        for (x, y) in (Settings.PrivacyAndSecurity.XiaZaiWenJianJia.P,
+                       Settings.PrivacyAndSecurity.XiaZaiWenJianJia.RangYingYongFangWenXiaZaiWenJianJia.P,
+                       Settings.PrivacyAndSecurity.XiaZaiWenJianJia.XiaZaiWenJianJiaFangWenQuanXian.P,
+                       Settings.PrivacyAndSecurity.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
+        pyautogui.click(Settings.Blank.P[0], Settings.Blank.P[1], interval=DEFAULT_INTERVAL)
+        pyautogui.scroll(-10000)
+        time.sleep(DEFAULT_INTERVAL)
+        for (x, y) in (Settings.PrivacyAndSecurity.YinYueKu.P,
+                       Settings.PrivacyAndSecurity.YinYueKu.YunXuYingYongFangWenNiDeYinYueKu.P,
+                       Settings.PrivacyAndSecurity.YinYueKu.YunXuFangWenCiSheBeiShangDeYinYueKu.P,
+                       Settings.PrivacyAndSecurity.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
+        pyautogui.click(Settings.Blank.P[0], Settings.Blank.P[1], interval=DEFAULT_INTERVAL)
+        pyautogui.scroll(-10000)
+        time.sleep(DEFAULT_INTERVAL)
+        for (x, y) in (Settings.PrivacyAndSecurity.TuPian.P,
+                       Settings.PrivacyAndSecurity.TuPian.RangYingYongFangWenTuPianKu.P,
+                       Settings.PrivacyAndSecurity.TuPian.TuPianKuFangWenQuanXian.P,
+                       Settings.PrivacyAndSecurity.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
+        pyautogui.click(Settings.Blank.P[0], Settings.Blank.P[1], interval=DEFAULT_INTERVAL)
+        pyautogui.scroll(-10000)
+        time.sleep(DEFAULT_INTERVAL)
+        for (x, y) in (Settings.PrivacyAndSecurity.ShiPin.P,
+                       Settings.PrivacyAndSecurity.ShiPin.RangYingYongFangWenShiPinKu.P,
+                       Settings.PrivacyAndSecurity.ShiPin.ShiPinKuFangWenQuanXian.P,
+                       Settings.PrivacyAndSecurity.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
+        pyautogui.click(Settings.Blank.P[0], Settings.Blank.P[1], interval=DEFAULT_INTERVAL)
+        pyautogui.scroll(-10000)
+        time.sleep(DEFAULT_INTERVAL)
+        for (x, y) in (Settings.PrivacyAndSecurity.WenJianXiTong.P,
+                       Settings.PrivacyAndSecurity.WenJianXiTong.RangYingYongFangWenWenJianXiTong.P,
+                       Settings.PrivacyAndSecurity.WenJianXiTong.WenJianXiTongFangWenQuanXian.P,
+                       Settings.PrivacyAndSecurity.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
+        pyautogui.click(Settings.Blank.P[0], Settings.Blank.P[1], interval=DEFAULT_INTERVAL)
+        pyautogui.scroll(-10000)
+        time.sleep(DEFAULT_INTERVAL)
+        for (x, y) in (Settings.PrivacyAndSecurity.PingMuJieTuBianKuang.P,
+                       Settings.PrivacyAndSecurity.PingMuJieTuBianKuang.RangYingYongGuanBiPingMuJieTuBianKuang.P,
+                       Settings.PrivacyAndSecurity.PingMuJieTuBianKuang.PingMuJieTuBianKuangSheZhiFangWenQuanXian.P,
+                       Settings.PrivacyAndSecurity.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
+        pyautogui.click(Settings.Blank.P[0], Settings.Blank.P[1], interval=DEFAULT_INTERVAL)
+        pyautogui.scroll(-10000)
+        time.sleep(DEFAULT_INTERVAL)
+        for (x, y) in (Settings.PrivacyAndSecurity.PingMuJieTuHePingMuLuZhi.P,
+                       Settings.PrivacyAndSecurity.PingMuJieTuHePingMuLuZhi.YunXuYingYongJieQuPingMuJieTuHePingMuLuZhi.P,
+                       Settings.PrivacyAndSecurity.PingMuJieTuHePingMuLuZhi.PingMuJieTuHePingMuLuZhiFangWenQuanXian.P,
+                       Settings.PrivacyAndSecurity.P):
+            pyautogui.click(x=x, y=y, interval=DEFAULT_INTERVAL)
+
     def windows_update_auto():
         pyautogui.click(Settings.WindowsUpdate.P[0], Settings.WindowsUpdate.P[1], interval=DEFAULT_INTERVAL)
 
@@ -54,17 +329,16 @@ def windows_settings_auto():
 
     open_full_size_windows_settings_window()
     windows_update_auto()
+    privacy_and_security_auto()
+    accessibility_auto()
+    gaming_auto()
     
     # todo: 不够稳定，有时会卡
     # todo: 坐标存在偏差需要调整
     # system_auto()
 
-
 def test():
     '''test'''
-    # pyautogui.hotkey('win', 'i')
-    # pyautogui.hotkey('win', 'up', interval=DEFAULT_INTERVAL)
-    windows_settings_auto()
-    
+
 if __name__ == '__main__':
     test()
--- position.py
+++ position.py
@@ -54,12 +54,222 @@ class Settings:
 
     class Gaming:
         P = (100, 742)
+        class GameBar:
+            P = (1479, 238)
+            class YunXuKongZhiQiDaKaiGameBar:
+                P = (2199, 218)
 
     class Accessibility:
         P = (100, 802)
+        class ShuBiaoZhiZhenYuChuKong:
+            P = (1494, 660)
+            class ChuKongXianShiQi:
+                class ShiYuanQuanGengShenGengDa:
+                    P = (859, 885)
+        class JiangShuRen:
+            P = (1507, 1099)
+            class JiangShuRenDeKuaiJieFangShi:
+                P = (2200, 446)
+        class JianPan:
+            P = (1488, 1200)
+            class NianZhiJian:
+                P = (1503, 331)
+                class NianZhiJianDeJianPanKuaiJieFangShi:
+                    P = (2197, 295)
+                class ZaiHangZhongAnLiangCiShiSuoDingKuaiJieJian:
+                    P = (2195, 519)
+            class ShaiXuanJian:
+                P = (1459, 445)
+                class ShaiXuanJianDeJianPanKuaiJieFangShi:
+                    P = (2195, 296)
 
     class PrivacyAndSecurity:
         P = (100, 862)
+        class ChangGui:
+            P = (1534, 662)
+            class YunXuWangZhanTongGuoFangWenWoDeYuYanLieBiaoLaiXianShiBenDiXiangGuanNeiRong:
+                P = (2195, 328)
+            class YunXuWindowsGenZongYingYongQiDongYiGaiJinKaiShiHeSouSuoNeiRong:
+                P = (2197, 437)
+            class ZaiSheZhiYingYongZhongWeiWoXianShiJianYiDeNeiRong:
+                P = (2197, 552)
+        class MoJiShuXieHeJianRuGeXingHua:
+            P = (1498, 880)
+            class ZiDingYiMoJiShuXieHeJianRuCiDian:
+                P = (2198, 296)
+        class ZhenDuanHeFanKui:
+            P = (1481, 985)
+            class ShanChuZhenDuanShuJu:
+                P = (1470, 890)
+                class ShanChu:
+                    P = (2114, 984)
+            class FanKuiPinLv:
+                P = (2132, 1081)
+                class CongBu:
+                    P = (2137, 1307)
+        class HuoDongLiShiJiLu:
+            P = (1488, 1103)
+            class HuoDongLiShiJiLu:
+                class ZaiCiSheBeiShangCunChuWoDeHuoDongLiShiJiLu:
+                    P = (2157, 314)
+            class QingChuCiZhangHuDeHuoDongLiShiJiLu:
+                P = (2131, 414)
+                class QingChu:
+                    P = (1090, 859)
+        class SouSuoQuanXian:
+            P = (1497, 1206)
+            class AnQuanSouSuo:
+                class GuanBi:
+                    P = (770, 550)
+            class YunNeiRongSouSuo:
+                class MicrosoftZhangHu:
+                    P = (783, 892)
+                class GongZuoHuoXueXiaoZhangHu:
+                    P = (783, 1073)
+            class LiShiJiLu:
+                class CiSheBeiShangDeSouSuoLiShiJiLu:
+                    P = (783, 1417)
+                class QingChuSheBeiSouSuoLiShiJiLu:
+                    P = (877, 1492)
+            class GengDuoSheZhi:
+                class XianShiSouSuoYaoDian:
+                    P = (784, 1014)
+        class WeiZhi:
+            P = (1500, 1488)
+            class YunXuYingYongFangWenWeiZhi:
+                P = (1518, 435)
+            class WeiZhiLiShiJiLu:
+                P = (2134, 650)
+            class YunXuWeiZhiFuGai:
+                P = (2197, 760)
+            class ZaiYingYongQingQiuWeiZhiShiFaChuTongZhi:
+                P = (2199, 982)
+        class YuYinJiHuo:
+            P = (1487, 485)
+            class YunXuYingYongFangWenYuYinJiHuoFuWu:
+                P = (1480, 405)
+                class Switch:
+                    P = (2154, 405)
+                class YunXuYingYongZaiSheBeiSuoDingShiShiYongYuYinJiHuoFuWu:
+                    P = (2153, 511)
+        class TongZhi:
+            P = (1467, 595)
+            class TongZhiFangWenQuanXian:
+                P = (2198, 218)
+            class RangYingYongFangWenTongZhi:
+                P = (2197, 328)
+        class ZhangHuXinXi:
+            P = (1475, 711)
+            class ZhangHuXinXiFangWenQuanXian:
+                P = (2196, 219)
+            class YunXuYingYongFangWenNiDeZhangHuXinXi:
+                P = (2153, 330)
+        class LianXiRen:
+            P = (1479, 818)
+            class LianXiRenFangWenQuanXian:
+                P = (2196, 217)
+            class YunXuYingYongFangWenNiDeLianXiRen:
+                P = (2199, 331)
+        class RiLi:
+            P = (1475, 921)
+            class RiLiFangWenQuanXian:
+                P = (2198, 221)
+            class YunXuYingYongFangWenNiDeRiLi:
+                P = (2200, 328)
+        class DianHuaHuJiao:
+            P = (1509, 1038)
+            class DianHuaHuJiaoFangWenQuanXian:
+                P = (2195, 216)
+            class YunXuYingYongBoDaDianHua:
+                P = (2197, 328)
+        class TongHuaJiLu:
+            P = (1491, 1150)
+            class HuJiaoLiShiJiLuFangWenQuanXian:
+                P = (2195, 219)
+            class YunXuYingYongFangWenNiDeHuJiaoLiShiJiLu:
+                P = (2198, 328)
+        class DianZiYouJian:
+            P = (1479, 1262)
+            class DianZiYouJianFangWenQuanXian:
+                P = (2198, 217)
+            class YunXuYingYongFangWenNiDeDianZiYouJian:
+                P = (2197, 326)
+        class RenWu:
+            P = (1478, 1373)
+            class RenWuFangWenQuanXian:
+                P = (2196, 218)
+            class YunXuYingYongFangWenNiDeRenWu:
+                P = (2198, 329)
+        class XiaoXi:
+            P = (1472, 1477)
+            class DuanXinFangWenQuanXian:
+                P = (2197, 218)
+            class YunXuYingYongDuQuDuanXin:
+                P = (2199, 329)
+        class WuXianShouFaQi:
+            P = (1508, 207)
+            class ShouYinJiKongJianFangWenQuanXian:
+                P = (2198, 219)
+            class RangYingYongKongZhiSheBeiWuXianDianShouFaQi:
+                P = (2198, 330)
+        class QiTaSheBei:
+            P = (1491, 318)
+            class YuWeiPeiDuiSheBeiTongXin:
+                P = (784, 339)
+        class YingYongZhenDuan:
+            P = (1489, 431)
+            class YingYongZhenDuanFangWenQuanXian:
+                P = (2196, 216)
+            class YunXuYingYongFangWenYuQiTaYingYongYouGuanDeZhenDuanXinXi:
+                P = (2195, 328)
+        class WenDang:
+            P = (1477, 643)
+            class WenDangKuFangWenQuanXian:
+                P = (2199, 218)
+            class RangYingYongFangWenWenDangKu:
+                P = (2153, 327)
+        class XiaZaiWenJianJia:
+            P = (1477, 754)
+            class XiaZaiWenJianJiaFangWenQuanXian:
+                P = (2194, 220)
+            class RangYingYongFangWenXiaZaiWenJianJia:
+                P = (2195, 329)
+        class YinYueKu:
+            P = (1474, 868)
+            class YunXuFangWenCiSheBeiShangDeYinYueKu:
+                P = (781, 370)
+            class YunXuYingYongFangWenNiDeYinYueKu:
+                P = (786, 635)
+        class TuPian:
+            P = (1484, 978)
+            class TuPianKuFangWenQuanXian:
+                P = (2198, 220)
+            class RangYingYongFangWenTuPianKu:
+                P = (2152, 330)
+        class ShiPin:
+            P = (1490, 1085)
+            class ShiPinKuFangWenQuanXian:
+                P = (2195, 218)
+            class RangYingYongFangWenShiPinKu:
+                P = (2152, 329)
+        class WenJianXiTong:
+            P = (1488, 1194)
+            class WenJianXiTongFangWenQuanXian:
+                P = (2196, 215)
+            class RangYingYongFangWenWenJianXiTong:
+                P = (2199, 330)
+        class PingMuJieTuBianKuang:
+            P = (1488, 1306)
+            class PingMuJieTuBianKuangSheZhiFangWenQuanXian:
+                P = (2196, 220)
+            class RangYingYongGuanBiPingMuJieTuBianKuang:
+                P = (2154, 331)
+        class PingMuJieTuHePingMuLuZhi:
+            P = (1495, 1416)
+            class PingMuJieTuHePingMuLuZhiFangWenQuanXian:
+                P = (2194, 217)
+            class YunXuYingYongJieQuPingMuJieTuHePingMuLuZhi:
+                P = (2156, 326)
 
     class WindowsUpdate:
         P = (100, 922)
